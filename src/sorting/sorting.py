63604606c7f9143b2c51d22d5b0852d448b4e4fe
Alfaroque Islam <alfaroque5@gmail.com>
2020-09-15 18:51:53 -0400

    CS Unit 1 Week 3 Assignment 2

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,14 +3,72 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    i, j, k =  0, 0, 0
+    while i < len(arrA) and j < len(arrB):
+        if arrA[i] < arrB[j]:
+            # The value from the left half has been used
+            merged_arr[k] = arrA[i]
+            # Move the iterator forward
+            i += 1
+        else:
+            merged_arr[k] = arrB[j]
+            j += 1
+            # Move to the next slot
+        k += 1
 
+        # For all the remaining values
+    while i < len(arrA):
+        merged_arr[k] = arrA[i]
+        i += 1
+        k += 1
 
+    while j < len(arrB):
+        merged_arr[k]=arrB[j]
+        j += 1
+        k += 1
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    lst = []
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              # The value from the left half has been used
+              arr[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k]=right[j]
+            j += 1
+            k += 1
+        #lst = merge(left, right)
 
 
     return arr
@@ -19,10 +77,10 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
