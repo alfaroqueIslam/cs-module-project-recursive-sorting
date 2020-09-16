114352524206d795340ae7386790f5e6d9ea2f7e
Alfaroque Islam <alfaroque5@gmail.com>
2020-09-16 18:19:50 -0400

    CS Unit 1 Week 3 Assignment 2

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -30,47 +30,14 @@ def merge(arrA, arrB):
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    lst = []
+    # lst = []
     if len(arr) > 1:
         mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
+        left = merge_sort(arr[:mid])
+        right = merge_sort(arr[mid:])
 
-        # Recursive call on each half
-        merge_sort(left)
-        merge_sort(right)
-
-        i = 0
-        j = 0
         
-        # Iterator for the main list
-        k = 0
-        
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-              # The value from the left half has been used
-              arr[k] = left[i]
-              # Move the iterator forward
-              i += 1
-            else:
-                arr[k] = right[j]
-                j += 1
-            # Move to the next slot
-            k += 1
-
-        # For all the remaining values
-        while i < len(left):
-            arr[k] = left[i]
-            i += 1
-            k += 1
-
-        while j < len(right):
-            arr[k]=right[j]
-            j += 1
-            k += 1
-        #lst = merge(left, right)
-
-
+        return merge(left, right)
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
